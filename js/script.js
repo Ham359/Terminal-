98ef7bd319ea2596dd67f9e9ea81ddbd303e0163
Ham359 <148823821+Ham359@users.noreply.github.com>
2025-04-24 12:15:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Activar los tooltips
+// Activar los tooltipsaaa
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
